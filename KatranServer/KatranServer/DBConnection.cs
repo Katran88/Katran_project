398aa71eae40362382e5ee9298d13631f4d5793d
Dmitriy Hunko <47152025+Katran88@users.noreply.github.com>
2020-12-11 21:31:41 +0300

    Update DBConnection.cs

--- KatranServer/KatranServer/DBConnection.cs
+++ KatranServer/KatranServer/DBConnection.cs
@@ -3,37 +3,6 @@ using System.Data.SqlClient;
 using Oracle.ManagedDataAccess.Client;
 namespace KatranServer
 {
-    //public class DBConnection
-    //{
-    //    private static SqlConnection connectionInstance;
-    //    const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Katran;Integrated Security=True";
-    //    private static object locker = new object();
-
-    //    private DBConnection()
-    //    {
-
-    //    }
-    //    public static SqlConnection getInstance()
-    //    {
-    //        lock (locker)
-    //        {
-    //            if (connectionInstance == null)
-    //            {
-    //                connectionInstance = new SqlConnection(connectionString);
-    //                connectionInstance.Open();
-    //                return connectionInstance;
-    //            }
-    //        }
-    //        return connectionInstance;
-    //    }
-
-    //    ~DBConnection()
-    //    {
-    //        connectionInstance.Close();
-    //    }
-
-    //}
-
     static class OracleDB
     {
         static string host = "192.168.137.8";
